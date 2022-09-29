6cf34d7d100289b926068b1d3784ddd21e813603
Yevgeniy Mineyev <fromarys@gmail.com>
2022-09-29 15:46:41 +0600

    feat: add components

--- youtube-client-app/src/app/app.module.ts
+++ youtube-client-app/src/app/app.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SearchItemComponent } from './search/search-item/search-item.component';
+import { SearchResultsComponent } from './search/search-results/search-results.component';
+import { SearchFieldComponent } from './search/search-field/search-field.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
+    SearchItemComponent,
+    SearchResultsComponent,
+    SearchFieldComponent,
   ],
   imports: [
     BrowserModule,
